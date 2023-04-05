1786748084fb00c6fe18c58ec70ca95eb7bbaf49
jcaddick62 <38961088+jcaddick62@users.noreply.github.com>
2023-04-04 20:14:37 -0400

    Updating event code
    2023incmp

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ingre",
+      "defaultValue": "2023incmp",
       "required": "true",
       "disabled": "true"
     },
